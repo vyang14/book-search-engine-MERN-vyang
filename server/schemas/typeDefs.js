29f8b14ae943c154699b5ee12059e09ffaea47dc
vyang14 <vyang1014888@gmail.com>
2022-08-24 10:42:53 -0400

    initial commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookid: ID!
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
