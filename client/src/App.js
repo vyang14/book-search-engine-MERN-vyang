0c3ab604c336126a0d9d6c9f0efcf7dd6f3efd8e
vyang14 <vyang1014888@gmail.com>
2022-08-29 01:13:55 -0400

    spelling error in client/src/App.js line 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
-import { setContext } from '@pollo/client/link/context'
+import { setContext } from '@apollo/client/link/context'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
